396fe15996a6801206dab4d4d1aaa5d3aa12a603
maina-0 <maina0@proton.me>
2025-02-16 19:35:48 +0300

    roy

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <GLFW/glfw3.h>
+#include <GL/gl.h> 
+#include <leif/leif.h>
+
 
 
 int main(){
@@ -8,14 +11,22 @@ int main(){
     
     glfwMakeContextCurrent(window);
 
+    lf_init_glfw(1280,720,window);
+
     while(!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.1f,0.1f,0.1f,0.1f);
+
+        lf_begin();
+        lf_text("fernando halllo kajajshskshs\n");
+        lf_end();
 
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
     glfwDestroyWindow(window);
     glfwTerminate();
-     
 
+    return 0;
+     
+}
